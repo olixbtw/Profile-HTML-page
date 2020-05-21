9f8a0bb2c599459991807d3fe0fab66649ff7af3
Oleksandr <oleksandr_revchuk@epam.com>
2020-05-21 18:33:28 +0300

    like button action button active

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,38 @@
 'use strict';
 
-const ctaForm = document.getElementById('ctaForm');
-const sliderControl = document.getElementById('sliderControl');
+// const ctaForm = document.getElementById('ctaForm');
+// const sliderControl = document.getElementById('sliderControl');
 
-sliderControl.addEventListener('click', changeSliderSlides)
-ctaForm.addEventListener('submit', formSubmitHandler)
+// sliderControl.addEventListener('click', changeSliderSlides)
+// ctaForm.addEventListener('submit', formSubmitHandler)
 
-function changeSliderSlides(e) {
-  let oldClass,
-    newClass,
-    controlsElement = e.target.parentElement,
-    sliderElement = e.target.parentElement.parentElement;
+// function changeSliderSlides(e) {
+//   let oldClass,
+//     newClass,
+//     controlsElement = e.target.parentElement,
+//     sliderElement = e.target.parentElement.parentElement;
 
-  collectionToArray(controlsElement.getElementsByTagName('li')).forEach((el, i) => {
-    if (e.target === el) newClass = `slider--item-active-${i + 1}`;
-  });
-  sliderElement.classList.forEach(el => {
-    if (/slider--item-active-\d/.test(el)) oldClass = el;
-  });
+//   collectionToArray(controlsElement.getElementsByTagName('li')).forEach((el, i) => {
+//     if (e.target === el) newClass = `slider--item-active-${i + 1}`;
+//   });
+//   sliderElement.classList.forEach(el => {
+//     if (/slider--item-active-\d/.test(el)) oldClass = el;
+//   });
 
-  if (oldClass !== newClass) {
-    collectionToArray(controlsElement.getElementsByTagName('li')).forEach(el => {
-      el.classList.remove('active');
-      if (e.target === el) el.classList.add('active');
-    });
-    sliderElement.classList.remove(oldClass);
-    sliderElement.classList.add(newClass);
-  }
-}
+//   if (oldClass !== newClass) {
+//     collectionToArray(controlsElement.getElementsByTagName('li')).forEach(el => {
+//       el.classList.remove('active');
+//       if (e.target === el) el.classList.add('active');
+//     });
+//     sliderElement.classList.remove(oldClass);
+//     sliderElement.classList.add(newClass);
+//   }
+// }
 
-function formSubmitHandler(e) {
-  e.preventDefault()
-  console.log(e.target)
-}
+// function formSubmitHandler(e) {
+//   e.preventDefault()
+//   console.log(e.target)
+// }
 
 // helper functions
 const collectionToArray = (collection) => {
@@ -41,4 +41,16 @@ const collectionToArray = (collection) => {
     arr[i] = collection[i];
   }
   return arr;
+}
+
+//////////////////////////////////////
+
+
+const likeBtn = document.getElementById('like_btn');
+const profileEl = document.getElementById('profile');
+
+likeBtn.addEventListener('click', likePage)
+
+function likePage() {
+  profileEl.classList.toggle('profile--liked');
 }
