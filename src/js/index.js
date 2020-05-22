7d58989e5f86538fc52704abd1db4b359c5fbf7f
Oleksandr <oleksandr_revchuk@epam.com>
2020-05-22 13:16:11 +0300

    added pages

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-const likeBtn = document.getElementById('like_btn');
-const profileEl = document.getElementById('profile');
-
-likeBtn.addEventListener('click', likePage)
-
-function likePage() {
-  profileEl.classList.toggle('profile--liked');
-}
